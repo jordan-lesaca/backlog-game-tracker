d654a93f8679d1fed42ee8c2a551bde1035f42d8
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-02-13 19:14:47 -0800

    add password and password confirmation to the user strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: [:create, :mygames]
 
     def index
         users = User.all
@@ -7,15 +6,17 @@ class UsersController < ApplicationController
     end
 
     def create #signup
-        user = User.create(user_params)
-        render json: user
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
     end
 
     def mygames 
         user = User.find(session[:user_id])
         userGames = user.games
+        session[:user_id] = user.id    
         render json: userGames
-      end
+    end
 
     def show
         user = User.find(session[:user_id])
@@ -26,7 +27,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :age)
+        params.permit(:username, :age, :password, :password_confirmation)
     end
 
 end
