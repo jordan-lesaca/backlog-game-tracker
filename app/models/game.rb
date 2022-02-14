742d16687fadbea0f8201a9dea4a94bbe3c0460d
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-02-13 19:15:46 -0800

    added validations to game model for title, year, and genre

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ApplicationRecord
     belongs_to :user
+
+    validates :title, presence: true
+    validates :year, presence: true
+    validates :year, length: { is: 4, message: "Please enter valid year " }
+    validates :year, numericality: { less_than: 2023}
+    validates :genre, presence: true
 end
